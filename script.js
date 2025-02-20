39ad2591386571503050152a42ff6cb3f9ef32f7
Алексей <alexseev617@gmail.com>
2025-02-20 14:39:20 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+// в языке программировании есть 8 типов данных
+// 6 примитивных
+const name = 'Aleksandr'
+const age = 28
+const isDeveloper = true
+const power = 99999999999999n
+const sign = Symbol()
+const something = undefined
+
+// два специальных
+const user = {
+	login: '123',
+	password: 'qwerty',
+}
+
+const data = null
+
+// Преобразуются в false
+
+console.log(Boolean(0))
+console.log(Boolean(NaN))
+console.log(Boolean(''))
+console.log(Boolean(null))
+console.log(Boolean(undefined))
+
+// Все остальные значения - в true
